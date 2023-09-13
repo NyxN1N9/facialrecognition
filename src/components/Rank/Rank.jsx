ad044df5615c2592fc01d4fd85552f8ca4a93c65
Chaos.Nyx <Chaos@chaos648@gmail.com>
2023-09-12 20:47:31 -0500

    first commit

---
+++ src/components/Rank/Rank.jsx
@@ -0,0 +1,14 @@
+import './Rank';
+
+const Rank = () => {
+    return (
+        <div className='white f3'>
+            {'Nyx, your current rank is...'}
+        <div className='white f1'>
+            {'#3'}
+        </div>      
+        </div>   
+    );
+}
+
+export default Rank;
