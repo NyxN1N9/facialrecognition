9fb3781667ae9805c47e2f6e3518b88b13270fb2
Chaos.Nyx <Chaos@chaos648@gmail.com>
2023-09-15 15:56:41 -0500

    chged files to js files

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -6,7 +6,7 @@ const ImageLinkForm = ({onInputChange, onButtonSubmit}) => {
             {'This magic Brain will detect faces in your pictures. Give it a try.'}
             <br></br>   
             <br></br>                  
-            <div className='form center pa4 br3 shadow-6'>
+            <div className='form center pa4 br3 shadow-6'> 
                 <input className='f4 pa2 w-70' 
                     type='text' onChange={onInputChange}/>
                 <button className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple' onClick={onButtonSubmit}>Detect</button>
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,10 +6,10 @@ const Logo = ()=>{
     return (
         <div className='ma4 mt0'>
            <Tilt className='Tilt br2 shadow-2' 
-                options={{ max: 40}}// max tilt rotation (degrees) 
-                style={{ height: 150, width: 150}}>
+                options={{ max: 200}}// max tilt rotation (degrees) 
+                style={{ height: 180, width: 180}}>
                 <div className='Tilt-inner pa3'>
-                    <img style={{ paddingTop: '10px'}} 
+                    <img style={{ paddingTop: '25px'}} 
                     alt='logo' src={brain}/>
                 </div>
             </Tilt>
