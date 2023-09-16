1b3279dfe268dcc1b03be2a8f2d8544e6d878355
Chaos.Nyx <Chaos@chaos648@gmail.com>
2023-09-15 23:28:48 -0500

    bg chgs

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
 /* eslint-disable no-mixed-spaces-and-tabs */
-import { Component } from 'react';
-import ParticlesBg from 'particles-bg';
-import Navigation from './components/Navigation/Navigation.js';
-import Logo from './components/Logo/Logo.js';
-import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm.js';
-import Rank from './components/Rank/Rank.js';
-import './App.css';
-import './index.css';
+import { Component } from "react";
+import ParticlesBg from "particles-bg";
+import Navigation from "./components/Navigation/Navigation.js";
+import Logo from "./components/Logo/Logo.js";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm.js";
+import Rank from "./components/Rank/Rank.js";
+import "./App.css";
+import "./index.css";
 
 // class component from video walk thru //
 class App extends Component {
-    render(){
-        return(
-            <>
-                <div className ="App">
-                    <ParticlesBg type="cobweb" num={200} bg={true} />, 
-                    <Navigation />,
-                    <Logo />,
-                    <Rank />,
-                    <ImageLinkForm />,
-                    {/*<FaceRecognition />*/}
-                </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div className="App">
+          <ParticlesBg color="#000000" num={200} type="cobweb" bg={true} />,
+          <Navigation />,
+          <Logo />,
+          <Rank />,
+          <ImageLinkForm />,{/*<FaceRecognition />*/}
+        </div>
+      </>
+    );
+  }
 }
 
 export default App;
