df16685331b42d2430b2b9e3c11385ce12749994
NyxN1N9 <125706841+NyxN1N9@users.noreply.github.com>
2023-11-11 11:37:08 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-/* eslint-disable no-undef */
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
-  return {
-    define: {
-      "process.env.SOME_KEY": JSON.stringify(env.SOME_KEY),
-    },
-    plugins: [react()],
-  };
-});
+export default defineConfig({
+  plugins: [react()],
+})
