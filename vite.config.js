3c92b8fbc07e2bd2917d01eee520526cd555c2f0
Nyx Neal <chaos648@gmail.com>
2023-11-19 16:34:12 -0600

    vite

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()]
+	plugins: [react()],
+	entry: './app/index.js',
+	output: './public/index.html'
 });
+
+/* 
+module.exports = {
+	entry: './app/main.js',
+	output: {
+			path: './dist',
+			filename: 'bundle.js'
+	}
+} 
+*/
